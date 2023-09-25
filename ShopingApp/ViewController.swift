6fcef6827aa181a952203936509e6072ac780fa8
sako-0715 <sakou.0715@gmail.com>
2023-09-25 23:43:48 +0900

    Initial Commit

---
+++ ShopingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopingApp
+//
+//  Created by 酒匂竜也 on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
